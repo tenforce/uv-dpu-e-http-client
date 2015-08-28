f613b90248e8f9813a339c9fb7d19d1a74e02b92
Vadym Roganin <vadym.roganin@tenforce.com>
2015-08-28 17:28:52 +0300

    File download plugin DPU: changes according to skype conversation

--- src/main/java/com/tenforce/uv/extractor/httpClient/httpClientVaadinDialog.java
+++ src/main/java/com/tenforce/uv/extractor/httpClient/httpClientVaadinDialog.java
@@ -7,7 +7,6 @@ import eu.unifiedviews.helpers.dpu.vaadin.dialog.AbstractDialog;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 
 /**
@@ -17,7 +16,12 @@ import java.util.Map;
  */
 public class httpClientVaadinDialog extends AbstractDialog<httpClientConfig_V1> {
 
-    private httpClientConfig_V1 configuration = new httpClientConfig_V1();
+    private TextField uriField;
+    private ComboBox methodMenu;
+    private TextArea bodyTextArea;
+    private List<HttpClientPair_V1> headersList = new ArrayList<>();
+    private List<HttpClientPair_V1> paramsList = new ArrayList<>();
+
 
     public httpClientVaadinDialog() {
         super(httpClient.class);
@@ -25,16 +29,21 @@ public class httpClientVaadinDialog extends AbstractDialog<httpClientConfig_V1> 
 
     @Override
     public void setConfiguration(httpClientConfig_V1 configuration) throws DPUConfigException {
-        this.configuration = configuration;
+        uriField.setValue(configuration.getUri());
+        methodMenu.setValue(configuration.getMethod());
+        bodyTextArea.setValue(configuration.getBody());
+        headersList = configuration.getHeaders();
+        paramsList = configuration.getParams();
     }
 
     @Override
     public httpClientConfig_V1 getConfiguration() throws DPUConfigException {
-        configuration.setUri("http://google.com");
-        configuration.setBody("body");
-        configuration.setMethod("get");
-        configuration.setHeaders(new ArrayList<HttpClientPair_V1>());
-        configuration.setParams(new ArrayList<HttpClientPair_V1>());
+        httpClientConfig_V1 configuration = new httpClientConfig_V1();
+        configuration.setUri(uriField.getValue());
+        configuration.setBody(bodyTextArea.getValue());
+        configuration.setMethod((String) methodMenu.getValue());
+        configuration.setHeaders(headersList);
+        configuration.setParams(paramsList);
         return configuration;
     }
 
@@ -49,20 +58,20 @@ public class httpClientVaadinDialog extends AbstractDialog<httpClientConfig_V1> 
 
 
         VerticalLayout upperLayout = new VerticalLayout();
-        final TextField uriField = new TextField();
+        uriField = new TextField();
         uriField.addValidator(new StringLengthValidator(
                 "URI Field cannot be empty!",
                 null, null, true));
         uriField.setWidth(500, Unit.PIXELS);
         Label uriLabel = new Label("URI:");
         Label methodLabel = new Label("Method:");
-        final ComboBox methodMenu = new ComboBox();
+        methodMenu = new ComboBox();
         methodMenu.setTextInputAllowed(false);
         methodMenu.setNullSelectionAllowed(false);
         methodMenu.addItem("GET");
         methodMenu.addItem("POST");
         methodMenu.setValue("GET");
-        final TextArea bodyTextArea = new TextArea("Body:");
+        bodyTextArea = new TextArea("Body:");
         bodyTextArea.setWidth(600, Unit.PIXELS);
 
         upperLayout.addComponent(uriLabel);
@@ -72,32 +81,15 @@ public class httpClientVaadinDialog extends AbstractDialog<httpClientConfig_V1> 
         upperLayout.addComponent(bodyTextArea);
 
         mainLayout.addComponent(upperLayout);
-        mainLayout.addComponent(buildParamsLayout("header", new ArrayList<HttpClientPair_V1>()));
-        mainLayout.addComponent(buildParamsLayout("param", new ArrayList<HttpClientPair_V1>()));
+        mainLayout.addComponent(buildParamsLayout("header", headersList));
+        mainLayout.addComponent(buildParamsLayout("param", paramsList));
         setCompositionRoot(mainLayout);
-        configuration.setUri(uriField.getValue());
-        configuration.setBody(bodyTextArea.getValue());
-
-        configuration.setMethod((String) methodMenu.getValue());
-        configuration.setHeaders(new ArrayList<HttpClientPair_V1>());
-        configuration.setParams(new ArrayList<HttpClientPair_V1>());
     }
 
-    private VerticalLayout buildParamsLayout(final String keyName, final List<HttpClientPair_V1> paramMap) {
+    private VerticalLayout buildParamsLayout(final String keyName, final List<HttpClientPair_V1> params) {
 
         // Building headers for a table
         final VerticalLayout mainLayout = new VerticalLayout();
-//        final Label headerHeader = new Label(keyName + ":");
-//        final Label valueHeader = new Label("value:");
-//        headerHeader.setWidth(300, Unit.PIXELS);
-//        valueHeader.setWidth(300, Unit.PIXELS);
-//        mainLayout.addComponent(new HorizontalLayout() {
-//            {
-//                addComponent(headerHeader);
-//                addComponent(valueHeader);
-//            }
-//        });
-
         final TextField keyText = new TextField();
         final TextField valueText = new TextField();
         keyText.setWidth(300, Unit.PIXELS);
@@ -117,10 +109,10 @@ public class httpClientVaadinDialog extends AbstractDialog<httpClientConfig_V1> 
                             Notification.show("Empty field!");
                             return;
                         }
-//                        if (!addParam(keyTextValue, valueTextValue, paramMap)) {
-//                            Notification.show("Duplicate " + keyName + "!");
-//                            return;
-//                        }
+                        if (!addParam(keyTextValue, valueTextValue, params)) {
+                            Notification.show("Duplicate or null" + keyName + "!");
+                            return;
+                        }
                         Label key = new Label(keyTextValue);
                         Label value = new Label(valueTextValue);
                         key.setWidth(300, Unit.PIXELS);
@@ -135,7 +127,7 @@ public class httpClientVaadinDialog extends AbstractDialog<httpClientConfig_V1> 
                             @Override
                             public void buttonClick(Button.ClickEvent clickEvent) {
                                 mainLayout.removeComponent(values);
-                                paramMap.remove(keyTextValue);
+                                params.remove(keyTextValue);
                             }
                         });
                         values.addComponent(removeButton);
@@ -157,12 +149,20 @@ public class httpClientVaadinDialog extends AbstractDialog<httpClientConfig_V1> 
         return paramsLayout;
     }
 
-    private boolean addParam(String header, String value, Map<String, String> paramMap) {
-        if (paramMap.containsKey(header)) {
-            return false;
-        } else {
-            paramMap.put(header, value);
-            return true;
+    private boolean addParam(String header, String value, List<HttpClientPair_V1> params) {
+        for (HttpClientPair_V1 pair : params) {
+            if (pair.getName().equals(header)) {
+                return false;
+            }
         }
+
+        if (header == null || header.isEmpty()) {
+            return false;
+        }
+        HttpClientPair_V1 pair = new HttpClientPair_V1();
+        pair.setName(header);
+        pair.setValue(value);
+        params.add(pair);
+            return true;
     }
 }
